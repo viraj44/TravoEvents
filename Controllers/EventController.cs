6f5244ad70c942b111abb16f4d8c151d53180925
viraj44 <51167965+viraj44@users.noreply.github.com>
2025-10-13 22:25:30 +0530

    Add files via upload

---
+++ Controllers/EventController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventManagerMVC.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult ListYourShows()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
